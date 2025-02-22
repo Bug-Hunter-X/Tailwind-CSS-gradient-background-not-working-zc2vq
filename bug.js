eb47c779c7b8754c4080b2c5fb0ee23aa40a7e8e
BugHunter-X <askzenai@gmail.com>
2025-02-22 15:46:39 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// in your component file
+<div className="bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg shadow-lg">
+  <h1>This is a gradient background</h1>
+</div>
+```
